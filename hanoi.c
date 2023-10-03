7d157229c5e499b34472786d7d0508cc2ed0be93
adrkoc <kocifaj3@uniba.sk>
2023-10-03 16:52:48 +0200

    chyba je opravena

--- hanoi.c
+++ hanoi.c
@@ -7,7 +7,7 @@ void hanoi(int n, int a, int b)
   else
   {
     hanoi(n - 1, a, 6 - a - b);
-    printf("posun kamen %d s %d na %d\n", n, a, b);
+    printf("posun kamen %d z %d na %d\n", n, a, b);
     hanoi(n - 1, 6 - a - b, b);
   }
 }
